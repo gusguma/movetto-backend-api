bae35e0b4c9991cae8bab73bc2d36a71a4839bc8
Gustavo Gutierrez <gusguma@gmail.com>
2020-03-17 12:28:00 +0100

    Update UserDao #8

--- src/main/java/com/movetto/api/dao/UserDao.java
+++ src/main/java/com/movetto/api/dao/UserDao.java
@@ -4,5 +4,7 @@ import com.movetto.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
-
+    User findUserByEmail(String email);
+    User findUserById(int id);
+    User findUserByUid(String uid);
 }
